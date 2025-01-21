b583451644151a74e703983115970d62cb838c5d
Robert Gregg <robwgregg@gmail.com>
2025-01-20 17:00:34 -0800

    Upload NMF method

--- src/MethylationReduction.jl
+++ src/MethylationReduction.jl
@@ -1,5 +1,17 @@
 module MethylationReduction
 
-# Write your package code here.
+using LinearAlgebra, SparseArrays, Random  # Base Packages
+using CUDA                                 # Allow calculations on GPU
+using Dates                                # Get the current time (for logging) 
+
+
+include("custom_nmf.jl")
+
+export 
+    message,
+    sparsity,
+    residual,
+    NMFCache,
+    solveNMF
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using MethylationReduction
 using Test
 
 @testset "MethylationReduction.jl" begin
-    # Write your tests here.
+    X = rand(100,100)
+    sort!(X,dims=1) #to give some structure to X
+    k = 10
+    nmf = NMFCache(X, k; α=0.2);
+    solveNMF(nmf, verbose=false, maxiter=200)
+
+    @test residual(nmf) < 0.05
 end
